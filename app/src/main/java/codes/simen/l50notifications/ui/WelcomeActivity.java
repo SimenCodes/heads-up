f745980207324a23fa0f9d19196a8184037496ed
SimenCodes <sb@simen.codes>
2014-09-02 14:38:10 +0200

    Fixed OverlayServiceCommon

--- app/src/main/java/codes/simen/l50notifications/ui/WelcomeActivity.java
+++ app/src/main/java/codes/simen/l50notifications/ui/WelcomeActivity.java
@@ -1,3 +1,18 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package codes.simen.l50notifications.ui;
 
 import android.app.Activity;
@@ -26,7 +41,7 @@ import codes.simen.l50notifications.util.Mlog;
 public class WelcomeActivity extends Activity {
     public static final String ACCESSIBILITY_SERVICE_NAME = "codes.simen.l50notifications/codes.simen.l50notifications.NotificationListenerAccessibilityService";
     public static final int REQUEST_CODE = 654;
-    private final String logTag = "L Notifications";
+    private final String logTag = "Heads-up";
     private static boolean isRunning = false;
     private SharedPreferences preferences = null;
 
