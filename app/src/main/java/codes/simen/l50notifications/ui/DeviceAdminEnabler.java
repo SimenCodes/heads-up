2606a57427ea5f105c0a42d37ae80d7f0c9a73c8
Simen Burud <sb@simen.codes>
2014-09-21 21:38:46 +0200

    Many users were confused about disabling the device admin setting. Now, they can disable it from the place they enabled it.

--- app/src/main/java/codes/simen/l50notifications/ui/DeviceAdminEnabler.java
+++ app/src/main/java/codes/simen/l50notifications/ui/DeviceAdminEnabler.java
@@ -1,9 +1,25 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package codes.simen.l50notifications.ui;
 
 import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import codes.simen.l50notifications.R;
@@ -18,6 +34,7 @@ public class DeviceAdminEnabler extends Activity {
 
         if (((DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE)).isAdminActive(AdminReceiver.getComponentName(this))) {
             Toast.makeText(this, "Already a device manager.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Settings.ACTION_SECURITY_SETTINGS));
             finish();
             return;
         }
